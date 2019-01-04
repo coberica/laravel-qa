0b609d32cfcbd220025c5841975ec919d8eec208
coberica <icely_teddy@hotmail.com>
2019-01-05 03:12:57 +0800

    Implement delete functionality for the question

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">
                     <div class="d-flex align-items-center">
-                        <h2>Edit Question</h2>
+                        <h1>{{$question->title}}</h1>
                         <div class="ml-auto">
                             <a href="{{route('questions.index')}}" class="btn btn-outline-secondary">Back to All Questions</a>
                         </div>
@@ -15,10 +15,7 @@
                 </div>
 
                 <div class="card-body">
-                    <form action="{{route('questions.update',$question->id)}}" method="post">
-                        @method('PUT')
-                        @include('questions._form',['buttonText'=>'Edit Question'])
-                    </form>
+                    {!!$question->body_html!!}
                 </div>
             </div>
         </div>
